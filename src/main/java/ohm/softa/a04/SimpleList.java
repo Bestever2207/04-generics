6c2426b187a0a21173db12f46e2137db92489cf0
lucas <lucas.horn.3@web.de>
2025-07-07 18:16:54 +0200

    zwischenstand

--- src/main/java/ohm/softa/a04/SimpleList.java
+++ src/main/java/ohm/softa/a04/SimpleList.java
@@ -1,10 +1,38 @@
 package ohm.softa.a04;
 
-public interface SimpleList extends Iterable {
+public interface SimpleList<T> extends Iterable {
 	/**
 	 * Add a given object to the back of the list.
 	 */
-	void add(Object o);
+	void add(T t);
+
+	default void addDefault(Class<T> tclass){
+        try {
+            this.add(tclass.newInstance());
+
+        } catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
+    }
+
+	default SimpleList<T> filter(SimpleFilter<T> filter){
+		SimpleList<T> result;
+
+        try {
+            result = (SimpleList<T>) getClass().newInstance();
+
+        } catch (InstantiationException | IllegalAccessException e) {
+			result = new SimpleListImpl<>();
+		}
+
+        for(T t : this){
+			if(filter.include(t)){
+				result.add(t);
+			}
+		}
+
+		return result;
+	}
 
 	/**
 	 * @return current size of the list
@@ -15,5 +43,4 @@ public interface SimpleList extends Iterable {
 	 * Generate a new list using the given filter instance.
 	 * @return a new, filtered list
 	 */
-	SimpleList filter(SimpleFilter filter);
 }
